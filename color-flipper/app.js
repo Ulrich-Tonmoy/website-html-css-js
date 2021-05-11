cbe306dcd587caec73bc0f8f99138369b549cfb9
Ulrich-Tonmoy <ulrichtonmoy1@gmail.com>
2021-05-11 18:18:45 +0600

    color flipper

---
+++ color-flipper/app.js
@@ -0,0 +1,9 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function () {
+    const randomNumber = Math.floor(Math.random() * colors.length);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
