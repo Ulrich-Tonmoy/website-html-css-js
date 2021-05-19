65636157edb29dfcf1bc8c5c0367525bd476869b
Ulrich-Tonmoy <ulrichtonmoy1@gmail.com>
2021-05-19 11:00:33 +0600

    cafe-menu
    cafe-menu dynamic  design

--- animated-counter/script.js
+++ animated-counter/script.js
@@ -9,9 +9,6 @@ counters.forEach(counter => {
     // Lower inc to slow and higher to slow
     const inc = target / speed;
 
-    // console.log(inc);
-    // console.log(count);
-
     // Check if target is reached
     if (count < target) {
       // Add inc to count and output in counter
--- navbar/app.js
+++ navbar/app.js
@@ -1,9 +1,3 @@
-// classList - shows/gets all classes
-// contains - checks classList for specific class
-// add - add class
-// remove - remove class
-// toggle - toggles class
-
 const navToggle = document.querySelector('.nav-toggle');
 const links = document.querySelector('.links');
 
