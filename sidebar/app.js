3044c61f6e115bcff01ed619c159491e8abff93f
Ulrich-Tonmoy <ulrichtonmoy1@gmail.com>
2021-08-31 02:59:21 +0600

    Update app.js

--- sidebar/app.js
+++ sidebar/app.js
@@ -1,7 +1,7 @@
 let arrow = document.querySelectorAll(".arrow");
 let sidebar = document.querySelector(".sidebar");
 let sidebarBtn = document.querySelector(".bx-menu");
-let sidebarLogoBtn = document.querySelector(".logo-details");
+let sidebarLogoBtn = document.querySelector(".bx-code-block");
 
 for (var i = 0; i < arrow.length; i++) {
     arrow[i].addEventListener("click", (e) => {
