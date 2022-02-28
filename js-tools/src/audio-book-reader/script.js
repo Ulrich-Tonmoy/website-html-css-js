3304a96577e1adae9751284bef785c5cc7620689
Ulrich-Tonmoy <ulrichtonmoy1@gmail.com>
2022-03-01 03:24:32 +0600

    audio-book-reader added

---
+++ js-tools/src/audio-book-reader/script.js
@@ -0,0 +1,33 @@
+const video = document.querySelector("video");
+const textEl = document.querySelector("[data-text]");
+const lang = "eng";
+
+async function setup() {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    video.srcObject = stream;
+
+    video.addEventListener("playing", async () => {
+        const worker = Tesseract.createWorker();
+        await worker.load();
+        await worker.loadLanguage(lang);
+        await worker.initialize(lang);
+
+        const canvas = document.createElement("canvas");
+        canvas.width = video.width;
+        canvas.height = video.height;
+
+        document.addEventListener("keypress", async (e) => {
+            if (e.code !== "Space") return;
+            canvas.getContext("2d").drawImage(video, 0, 0, video.width, video.height);
+            const {
+                data: { text },
+            } = await worker.recognize(canvas);
+
+            speechSynthesis.speak(new SpeechSynthesisUtterance(text.replace(/\s/g, " ")));
+
+            textEl.textContent = text;
+        });
+    });
+}
+
+setup();
