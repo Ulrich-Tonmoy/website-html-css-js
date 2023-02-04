7891179a229ca2b804cfaac67692e5db029889f3
Tonmoy <ulrichtonmoy1@gmail.com>
2023-02-04 20:41:14 +0600

    loading-v60

---
+++ css-temps/src/loading-v60/main.js
@@ -0,0 +1,5 @@
+document.querySelector("button").addEventListener("click", function () {
+    document.body.innerHTML += '<div class="dv"></div>';
+});
+
+/* Illustration taken from: https://dribbble.com/shots/1912258-Darth-Vader-pixel-art*/
